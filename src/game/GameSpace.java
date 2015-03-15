ed7edfc11976b60758a79356940a5c9977216650
faradayDK <faradaylv@yandex.com>
2015-03-15 22:11:57 +0100

    Global variables for bricks assigned

--- src/game/GameSpace.java
+++ src/game/GameSpace.java
@@ -9,10 +9,13 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 //Package "Basic Game" for 2D game development
 
-//Main script 
+
+
 public class GameSpace extends BasicGame
 {
-
+	//Global variables
+	Brick [] bricks = new Brick[10];
+	
 	public GameSpace(String gameName)
 	{
 		super(gameName);
@@ -21,7 +24,8 @@ public class GameSpace extends BasicGame
 	@Override
 	public void init(GameContainer gc) throws SlickException
 	{
-		
+		bricks[0]= new Brick(2, 400, 400);
+		bricks[1]= new Brick(2, 500, 500);
 	}
 
 	@Override
@@ -33,28 +37,28 @@ public class GameSpace extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-	Brick [] bricks = new Brick[2];
-	bricks[0]= new Brick(2, 400, 400);
-	bricks[1]= new Brick(2, 500, 500);
-    for(int i = 0 ; i<bricks.length; i++){
-	g.drawRect(bricks[i].X() ,bricks[i].Y() , bricks[i].Length() , bricks[i].Width() );	
+       for(int i = 0 ; i<bricks.length; i++){
+	       g.drawRect(bricks[i].X() ,bricks[i].Y() , bricks[i].Length() , bricks[i].Width() );	
     }
 	
 	}
-
+	
+	
 	//Main method
-	public static void main(String[] args)
-	{
-		try
+		public static void main(String[] args)
 		{
-			AppGameContainer appgc;
-			appgc = new AppGameContainer(new GameSpace("Breakout"));
-			appgc.setDisplayMode(1280, 720, false);
-			appgc.start();
+			try
+			{
+				AppGameContainer appgc;
+				appgc = new AppGameContainer(new GameSpace("Breakout"));
+				appgc.setDisplayMode(1280, 720, false);
+				appgc.start();
+			}
+			catch (SlickException ex)
+			{
+				Logger.getLogger(GameSpace.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
-		catch (SlickException ex)
-		{
-			Logger.getLogger(GameSpace.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+
+	
 }
