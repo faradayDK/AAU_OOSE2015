60acd5f2a75be99fcd76b29e18ff7cd083d5bc18
faradayDK <faradaylv@yandex.com>
2015-03-15 20:07:54 +0100

    Improved GameSpace

--- src/game/GameSpace.java
+++ src/game/GameSpace.java
@@ -1,5 +1,15 @@
 package game;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
 public class GameSpace {
 
+	public GameSpace(String gameName){
+		
+	}
 }
