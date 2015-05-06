43259172e2680d077c882f3e34e15bf0a6e46667
KriskaSosiska <kkonov13@student.aau.dk>
2015-05-06 17:32:32 +0200

    Lives implemented
    added some pictures which pop up if you lost one life. Timer implemented
    to make it possible to read what's written on pictures.

---
+++ src/game/Timer.java
@@ -0,0 +1,34 @@
+package game;
+
+public class Timer {
+	private int time, time2, delay2;
+	
+	public int delay;
+	
+	public Timer(int time, int delay){
+		this.time = time;
+		this.time2 = time;
+		this.delay = delay;
+		this.delay2 = delay;
+	}
+
+	public void timerStart(){
+		 if(time>0){
+			 time--;
+		 }
+		 else{
+			 delay--;
+			 resetTime();
+		 }
+	}
+	public void resetTime(){
+		time = time2;
+	}
+	public int GetDelay(){
+		return delay;
+	}
+	public void resetDelay(){
+		delay = delay2;
+	}
+
+}
